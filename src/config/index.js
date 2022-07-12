7f55f251221bac759f44a7d63cae4718028ce020
Adrian Alex <alextravc.342@gmail.com>
2022-07-12 09:18:24 -0500

    .example.env added

--- src/config/index.js
+++ src/config/index.js
@@ -4,5 +4,5 @@ import { config } from 'dotenv';
 
 if (process.env.NODE_ENV !== 'producction') config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3300;
 export const MONGODB_URI = process.env.MONGODB_URI;
